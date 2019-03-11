24981dc7cbc6e213c40bc765a677c10938026353
Sebass van Boxel <svboxel@gmail.com>
2019-03-11 22:46:08 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:10.11.0-alpine
+
+COPY ./src /action
+
+ENTRYPOINT ["/action/entrypoint.sh"]
+
+# LABEL "com.github.actions.name"="NodeJS Action Template"
+# LABEL "com.github.actions.description"="Template for new GitHub Actions running NodeJS"
+# LABEL "com.github.actions.icon"="book-open"
+# LABEL "com.github.actions.color"="green"
+# LABEL "repository"="https://github.com/bitoiu/node-js-action-template"
+# LABEL "homepage"="https://github.com/bitoiu/node-js-action-template"
+# LABEL "maintainer"="https://github.com/bitoiu"
---
+++ src/action.js
@@ -0,0 +1,114 @@
+// Requires and variable definition
+const log = require('loglevel')
+const fs = require('fs')
+
+// Uncomment this if you want to check if your local env variables are being set
+// console.dir(process.env)
+
+// // Method that loads the workflow event JSON payload
+// let getEventData = function () {
+//   return JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
+// }
+
+// // Set default log level or read the environment setting
+log.setLevel(process.env.LOG_LEVEL || 'info')
+
+// Print out the event data
+// log.trace(`Event data: ${JSON.stringify(getEventData())}`)
+
+// Write your code :)
+log.info("You are ready to start coding your GitHub Action")
+
+const request = require("request");
+
+class SpotifyAction {
+  constructor({ token }) {
+    this.endpoints = {
+      api_uri: 'https://api.spotify.com/v1',
+      searchSong: '/search', 
+      playSong: '/me/player/play'
+    }
+    
+    this.token = token;
+  }
+  
+  get headers() {
+    return {
+      'Authorization': `Bearer ${this.token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json' 
+    }
+  }
+
+  getEndpoint(method) {
+    return `${this.endpoints.api_uri}${this.endpoints[method]}`
+  }
+  
+  searchSong(query) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: `${this.getEndpoint('searchSong')}?q=${query}%20&type=track&limit=1`,
+        method: 'GET',
+        headers: this.headers
+      };
+      
+      request(options, (err, res, body) => {
+        if (err) {
+          log.error('search request error: ', err)
+          reject();
+        }
+        
+        var headers = res.headers
+        var statusCode = res.statusCode
+        log.debug(`---- debug play request ----`)
+        log.debug('headers', headers)
+        log.debug('statusCode', statusCode)
+        log.debug('body', body)
+        log.debug(`---- end debug play request ----`)
+        
+        const song = JSON.parse(body).tracks.items[0].uri
+        log.info(`found ${song}`);
+        resolve(song);
+      })
+    });
+  }
+  
+  playSong(song) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: this.getEndpoint('playSong'),
+        method: 'PUT',
+        headers: this.headers,
+        json: {
+          "uris": [song]
+        }
+      };
+      
+      request(options, (err, res, body) => {
+        if (err) {
+          log.error('play request error: ', err)
+          reject();
+        }
+        
+        var headers = res.headers
+        var statusCode = res.statusCode
+        log.debug(`---- debug play request ----`)
+        log.debug('headers', headers)
+        log.debug('statusCode', statusCode)
+        log.debug('body', body)
+        log.debug(`---- end debug play request ----`)
+        
+        log.info(`Playing ${song}`);
+        resolve();
+      })
+    })
+  }
+};
+
+const token = process.env.token;
+
+const Spotify = new SpotifyAction({ token });
+
+Spotify.searchSong('Muse').then(song => Spotify.playSong(song));
+
+
